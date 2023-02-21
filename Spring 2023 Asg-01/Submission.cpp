45382df2a220eda9200fd7f15c8f1afc8bf67e18
Ad <thenewadeel@gmail.com>
2023-02-21 23:40:39 +0500

    init

---
+++ Spring 2023 Asg-01/Submission.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+char* removeSentence(char* Para, char* input)
+{
+
+}
+
+bool FindSubString(char* Str, char* substr, int& start, int& end)
+{
+
+}
+
+char*** ConvertToDynamic(char arr[], int x, int y, int z) //Converts 1D Array to 3D dynamically allocated array
+{
+
+}
+
+bool MatchString3DArray(char*** mat, int xSize, int ySize, int zSize, char * input, int**& resultMat, int& colSize)
+{
+
+}
+
+void DeleteArray(char***& arr, int x, int y, int z)
+{
+    
+}
+
+bool isperfectNumber(int n)
+{
+
+}
+
+int findVowels(char* str)
+{
+
+}
+
+int pascal(int row, int col)
+{
+
+}
+
+void PrintPattern1(int start, int end)
+{
+
+}
+
+void printHollowDiamond(int n)
+{
+
+}
+
+void PrintPattern2(int , int )
+{
+
+}
+
+void startSnakeGame() // controller of the game
+{
+
+}

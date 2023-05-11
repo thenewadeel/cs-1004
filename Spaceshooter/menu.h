40c7dbd7800c866a3379aa9ae5c3ffc2a7edca75
Ad <thenewadeel@gmail.com>
2023-05-11 17:42:06 +0500

    menu revamped

--- Spaceshooter/menu.h
+++ Spaceshooter/menu.h
@@ -2,14 +2,25 @@
 #include "game.h"
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <ctime>
 #include <string>
 class Menu {
   string defaultMenuStrings[4] = {"Go to Battle!", "Options", "Instructions",
                                   "Exit Game"};
+  string storyline =
+      "Our galaxy is attacked by enemies. The enemy invaders \n"
+      "beat our squad, they destroyed all of galactica. Space \n"
+      "team is waiting for your order. Please command the ship \n"
+      "to protect the galaxy and the surrounding asteroids ";
+  Clock grandClock;
 
 public:
-  Sprite background; // Game background sprite
+  Sprite background;     // Game background sprite
+  GameEntity menuCursor; // Menu cursor sprite
   Texture bg_texture;
   sf::Font font;
   struct gameOptions {
@@ -21,10 +32,13 @@ public:
     menuOption_Instructions,
     menuOption_Quit,
     menuOption_None
-  } defaultMenuOptions;
+  } defaultMenuOptions,
+      currentOption, selectedOption;
+  int menuStops(menuOptions index) { return 160 + 75 * index; }
   // add menu attributes here
   Menu() {
-    menuOptions selectedOption = menuOption_None;
+    selectedOption = menuOption_None;
+    currentOption = menuOption_StartGame;
     if (!font.loadFromFile("font1.otf")) {
       // error...
       cout << "ERR loading font";
@@ -32,6 +46,10 @@ public:
     bg_texture.loadFromFile("img/background.jpg");
     background.setTexture(bg_texture);
     background.setScale(1, 1);
+
+    menuCursor.setSprite("img/PNG/playerShip3_red.png");
+    menuCursor.setRotation(90);
+    menuCursor.setScale(0.75, 0.75);
     // constructors body
   }
   sf::Text stylizeText(sf::Text &obj, int fontSize = 12,
@@ -56,7 +74,7 @@ public:
   sf::Text applyStyles(sf::Text &obj, textStyles stl) {
     switch (stl) {
     case textStyle_Title:
-      return stylizeText(obj, 72, sf::Color::Red, sf::Color::White, 8);
+      return stylizeText(obj, 150, sf::Color::Red, sf::Color::White, 8);
     case textStyle_Menu:
       return stylizeText(obj, 45, sf::Color::White, sf::Color::Red, 1);
     case textStyle_Info:
@@ -67,13 +85,53 @@ public:
       return stylizeText(obj, 24, sf::Color::White);
     }
   }
+  void drawMainMenu(RenderWindow &window) {
+    window.clear(Color::Black); // clears the screen
+    window.draw(background);    // setting background
+    sf::Text titleText;
+    titleText.setString("Space Shooter!");
+    titleText.move(25, -20);
+    //   textx = stylizeText(textx);
+    titleText = applyStyles(titleText, textStyle_Title);
+    window.draw(titleText);
+    for (int i = 0; i < 4; i++) {
+      sf::Text menuText;
+      menuText.setString(defaultMenuStrings[i]);
+      menuText.move(SCREEN_WIDTH / 3, menuStops((menuOptions)i));
+      menuText = applyStyles(menuText, textStyle_Menu);
+      window.draw(menuText);
+    }
+    menuCursor.setPosition(40 + SCREEN_WIDTH / 4, menuStops(currentOption) - 5);
+    menuCursor.draw(window, 0, 0);
+
+    window.display(); // Displying all the sprites
+  }
+  void drawInstrsMenu(RenderWindow &window) {
+    float timer = clock();
+    window.clear(Color::Black); // clears the screen
+    window.draw(background);    // setting background
+    sf::Text storylineText;
+    storylineText.setString(storyline);
+    int yLocation = SCREEN_HEIGHT - 100 -
+                    (int)(50 * grandClock.getElapsedTime().asSeconds()) %
+                        (SCREEN_HEIGHT - 150);
+    storylineText.move(25, yLocation);
+    //   textx = stylizeText(textx);
+    storylineText = applyStyles(storylineText, textStyle_Info);
+    window.draw(storylineText);
+
+    window.display(); // Displying all the sprites
+  }
   void display_menu() {
+    Game g;
     srand(time(0));
     RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), title);
-    Clock clock;
+    Clock clock, actionClock;
     float timer = 0;
     // cout << makeText().getString();
     while (window.isOpen()) {
+      float actionCooldownTime = 250;
+      float actionTime = actionClock.getElapsedTime().asMilliseconds();
       float time = clock.getElapsedTime().asSeconds();
       clock.restart();
       timer += time;
@@ -84,45 +142,50 @@ public:
           window.close();            // close the game
       }
 
-      if (Keyboard::isKeyPressed(Keyboard::Up) ||
-          Keyboard::isKeyPressed(Keyboard::W)) // If up key is pressed
-        ;
+      //   if (Keyboard::isKeyPressed(Keyboard::Up) ||
+      //       Keyboard::isKeyPressed(Keyboard::W)) // If up key is pressed
+      //   {
+      //     if (actionTime > actionCooldownTime) {
+      //       actionClock.restart();
+      //       selectedOption = (menuOptions)((selectedOption - 1) % 4);
+      //     }
+      //   }
       if (Keyboard::isKeyPressed(Keyboard::Down) ||
           Keyboard::isKeyPressed(Keyboard::S)) // If down key is pressed
-        ;
-
-      ////////////////////////////////////////////////
-      /////  Call your functions here            ////
-      //////////////////////////////////////////////
-      window.clear(Color::Black); // clears the screen
-      window.draw(background);    // setting background
-      sf::Text titleText;
-      titleText.setString("Space Shooter!");
-      titleText.move(SCREEN_WIDTH / 4, 50);
-      //   textx = stylizeText(textx);
-      titleText = applyStyles(titleText, textStyle_Title);
-      window.draw(titleText);
-      for (int i = 0; i < 4; i++) {
-        sf::Text menuText;
-        menuText.setString(defaultMenuStrings[i]);
-        menuText.move(SCREEN_WIDTH / 3, (150 + 60 * i));
-        menuText = applyStyles(menuText, textStyle_Menu);
-        window.draw(menuText);
+      {
+        if (actionTime > actionCooldownTime) {
+          actionClock.restart();
+          currentOption = (menuOptions)((currentOption + 1) % 4);
+        }
+      }
+      if (Keyboard::isKeyPressed(Keyboard::Enter) ||
+          Keyboard::isKeyPressed(Keyboard::Space)) // If down key is pressed
+      {
+        if (actionTime > actionCooldownTime) {
+          actionClock.restart();
+          //   selectedOption = (menuOptions)((selectedOption + 1) % 4);
+          selectedOption = currentOption;
+        }
       }
 
-      //   titleText.setString("Tip: close the game & go study");
-      //   titleText.move(0, 100);
-      //   //   textx = stylizeText(textx);
-      //   titleText = applyStyles(titleText, textStyle_Tip);
-      //   window.draw(titleText);
-
-      //   titleText.setString("Info: Wow it works");
-      //   titleText.move(0, 100);
-      //   //   textx = stylizeText(textx);
-      //   titleText = applyStyles(titleText, textStyle_Info);
-      //   window.draw(titleText);
-
-      window.display(); // Displying all the sprites
+      switch (selectedOption) {
+      default:
+        drawMainMenu(window);
+        break;
+      case menuOption_Instructions:
+        drawInstrsMenu(window);
+        break;
+      case menuOption_Quit:
+        window.close();
+        break;
+      case menuOption_GameOptions:
+        cout << "Not implemented yet";
+        break;
+      case menuOption_StartGame:
+        window.close();
+        g.start_game();
+        break;
+      }
 
       //   Game g;
       // display menu screen here

b20611dcfc6adfe87734245498fdee465fc81c6f
Ad <thenewadeel@gmail.com>
2023-05-17 01:35:28 +0500

    en bombs tick

--- Spaceshooter/enemy.h
+++ Spaceshooter/enemy.h
@@ -1,3 +1,4 @@
+#include "bullet.h"
 #include "gameEntity.h"
 #include <SFML/Config.hpp>
 #include <SFML/Graphics.hpp>
@@ -8,20 +9,46 @@
 #include <SFML/System/String.hpp>
 #include <cstdlib>
 #include <string.h>
+#include <vector>
 using namespace sf;
 #ifndef ENEMY
 #define ENEMY
 class Enemy : public GameEntity {
   Texture tex;
+  std::vector<Bullet> bombs;
+  Clock actionClock;
 
 public:
   float speed = 0.25;
-
+  float fireInterval = 500;
+  int fireProbability = 10;
+  int maxBombs = 2;
   Enemy() {
     this->setSprite("img/enemy_1.png");
     this->setScale(0.5, 0.5);
   }
-  void fire() {}
+  void tick() {
+    float actionTime = actionClock.getElapsedTime().asMilliseconds();
+    if (actionTime > fireInterval && bombs.size() < maxBombs) {
+      actionClock.restart();
+      if ((rand() % 100) < fireProbability)
+        fire();
+    }
+    eraseDeadEntities(bombs);
+  }
+  void eraseDeadEntities(std::vector<Bullet> &en) {
+    en.erase(std::remove_if(en.begin(), en.end(),
+                            [](GameEntity x) { return !x.isAlive(); }),
+             en.end());
+  }
+  void fire() {
+    Bullet b;
+    b.setSprite("img/enemy_laser.png");
+    b.setHeading(Vector2f(0, 1));
+    b.setPosition(sprite.getPosition().x + (w * scaleX / 2),
+                  sprite.getPosition().y + (h * scaleY / 2));
+    bombs.push_back(b);
+  }
   void movement(std::string s) {
     float delta_x = 0, delta_y = 0;
     // if(s=="l")
@@ -44,17 +71,12 @@ public:
 
     sprite.move(delta_x, delta_y);
   }
-  // void draw(RenderWindow &window, bool drawBounds = false,
-  //           bool drawHealth = false) {
-  //   window.draw(sprite);
-  //   if (drawHealth) {
-  //     RectangleShape healthRect = RectangleShape(Vector2f(health * w / 100,
-  //     2)); healthRect.setPosition(sprite.getPosition());
-  //     healthRect.setFillColor(sf::Color::Green);
-  //     window.draw(healthRect);
-  //   }
-  //   if (drawBounds)
-  //     window.draw(boundingRect());
-  // }
+  void draw(RenderWindow &window, bool drawBounds = true) {
+    for (int i = 0; i < bombs.size(); i++) {
+      bombs.at(i).tick();
+      bombs.at(i).draw(window);
+    }
+    GameEntity::draw(window);
+  }
 };
 #endif
